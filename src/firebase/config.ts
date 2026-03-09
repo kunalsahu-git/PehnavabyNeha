e08787e918cf09640b7c66add386138bcd185ad2
KUNAL SAHU <kunalsahubb@gmail.com>
2026-03-09 15:41:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-828369372-9952c",
+  "appId": "1:1023713814509:web:396f21d799ec2d608229e4",
+  "apiKey": "AIzaSyB4OCtrAZfO0ndL2dE9X4cUMBiMW0x_7Xs",
+  "authDomain": "studio-828369372-9952c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1023713814509"
+};
