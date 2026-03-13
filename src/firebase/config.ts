8bffbe3e197e332437e894a457e2a9a81407bb00
KUNAL SAHU <kunalsahubb@gmail.com>
2026-03-13 07:05:45 +0000

    ok we are facing the ame issue again , here we need to frst have a firba

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,12 @@
+/**
+ * Firebase Project Configuration
+ * 
+ * To use your own Firebase project:
+ * 1. Go to https://console.firebase.google.com/
+ * 2. Create a project and add a Web App.
+ * 3. Copy the firebaseConfig object and replace the values below.
+ * 4. Enable Firestore, Authentication (Anonymous/Email), and Storage.
+ */
 export const firebaseConfig = {
   "projectId": "studio-828369372-9952c",
   "appId": "1:1023713814509:web:396f21d799ec2d608229e4",
